9da204ae10d0fb2038b6d496221ddc26540bc6f7
TrineKnudsen <trinesaknudsen@hotmail.com>
2021-08-26 11:47:47 +0200

    update StringConstants

--- TSAK.PetShopComp.2021.UI/StringConstants.cs
+++ TSAK.PetShopComp.2021.UI/StringConstants.cs
@@ -4,16 +4,24 @@
     {
         public const string WelcomeGreeting = "Welcome to the Petshop";
         public const string PleaseSelectMain = "Please select one of the Items below to continue";
+        
+        //Menu
         public const string ViewAllPets = "Select 1 to view all pets in the Pet shop";
         public const string CreatePet = "Select 2 to create a new pet";
 
-        public const string CreatePetGreeting =
-            "Welcome to the pet factory, please follow the instructions to create your new pet";
-
+        //Create Pet
+        public const string CreatePetGreeting = "Welcome to the pet factory, please follow the instructions to create your new pet";
         public const string PetName = "Please enter a name for the pet";
         public const string PetColor = "Please enter the color of the pet";
         public const string PetBirthdate = "Please enter birthday of the pet (DD-MM-YYYY)";
         public const string PetSoldDate = "Please enter sold date of the pet (DD-MM-YYYY)";
-        public static string PetPrice = "Please enter the price of the pet";
+        public const string PetPrice = "Please enter the price of the pet";
+        
+        //Update pet
+        public const string Type = "Please type in ";
+
+        //Delete pet
+
+        //Sort by type
     }
 }
