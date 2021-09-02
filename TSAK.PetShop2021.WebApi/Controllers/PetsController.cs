c11c33c2fd002d6e94b4a087e4903d00dd36bb0e
TrineKnudsen <trinesaknudsen@hotmail.com>
2021-09-02 18:17:36 +0200

    initial commit restApi

---
+++ TSAK.PetShop2021.WebApi/Controllers/PetsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TSAK.PetShopComp._2021.IService;
+using TSAK.PetShopComp._2021.Model;
+
+
+namespace TSAK.PetShop2021.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class PetsController : ControllerBase
+    {
+        private readonly IPetService _petService;
+
+        public PetsController(IPetService petService)
+        {
+            _petService = petService;
+        }
+        
+        [HttpPost]
+        public Pet Create(Pet pet)
+        {
+            return _petService.CreatePet(pet);
+        }
+
+        [HttpGet]
+        public List<Pet> GetAll()
+        {
+            return _petService.GetPets();
+        }
+    }
+}
